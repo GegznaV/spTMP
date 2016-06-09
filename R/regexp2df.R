67a0c03b4c1ee518a80f143838c33633696ad3b7
GegznaV <GegznaV@gmail.com>
2016-06-09 16:29:32 +0300

    v0.0.9

--- R/regexp2df.R
+++ R/regexp2df.R
@@ -7,8 +7,7 @@
 #' \bold{unnamed tokens} of regular expressions and convert the result to a
 #' data frame.
 #'
-#' @note Call to function \code{gregexpr} with parameter \code{perl = TRUE}
-#' is used.
+#' @export
 #'
 #' @param text The text to be parsed: a character vector where matches are
 #'        sought, or an object which can be coerced by \code{as.character}
@@ -21,7 +20,7 @@
 #'
 #' @inheritParams base::gregexpr
 #'
-#' @return A data frame with parsed information.
+#' @return A data frame with extracted information.
 #'
 #' @details The syntax how to use \bold{named tokens} is: \cr
 #' '\emph{...}\bold{(?<}\emph{...}>\emph{...})\emph{...}' \cr
@@ -39,90 +38,17 @@
 #' \bold{Unnamed tokens} are simply groups of expressions inside round
 #' brackets "\code{()}". The expressions outside brackets are not captured.
 #'
-#' @export
 #'
 #' @seealso More about regular expressions used in R: \link[base]{regex}\cr
+#'
 #'          Website handy for creating and testing Perl-like regular expressions
 #'          (library \emph{pcre}, version 1, not 2)
 #'          \url{https://regex101.com/r/dS3iP1/1#pcre} \cr
 #'
 #'          Functions \code{\link[base]{gregexpr}},
-#'          \code{\link{regcapturedmatches}},
 #'          operator from package \pkg{magrittr} \code{\link[magrittr]{\%>\%}}.
-#'
-#' @examples
-#'
-#' text1 <- c("A_111  B_aaa",
-#'               "A_222  B_bbb",
-#'               "A_333  B_ccc",
-#'               "A_444  B_ddd",
-#'               "A_555  B_eee")
-#'
-#' # Named tokens
-#' pattern1_named_tokens <- 'A_(?<Part_A>.*)  B_(?<Part_B>.*)'
-#'
-#' regexp2df(text1, pattern1_named_tokens)
-#' ##     Part_A Part_B
-#' ## 1    111    aaa
-#' ## 2    222    bbb
-#' ## 3    333    ccc
-#' ## 4    444    ddd
-#' ## 5    555    eee
-#'
-#' # Unnamed tockens - groups inside brackets:
-#' pattern1_unnamed_tokens <- 'A_(.*)  B_(.*)'
-#'
-#' regexp2df(text1, pattern1_unnamed_tokens)
-#' ##       X     X.1
-#' ## 1    111    aaa
-#' ## 2    222    bbb
-#' ## 3    333    ccc
-#' ## 4    444    ddd
-#' ## 5    555    eee
-#'
-#'
-#' #----------------------------------------------------------
-#' # Wrong. There must be NO SPACES in token's name:
-#'
-#' \donttest{
-#' \dontrun{
-#' pattern2 <- 'A (?<Part A>.*)  B (?<Part B>.*)'
-#' regexp2df(text1, pattern2)
-#'
-#' ## Error ...
-#'
-#' }}
-#' #----------------------------------------------------------
-#' text3 <- c("sn555 ID_O20-5-684_N52_2_Subt2_01.",
-#'               "sn555 ID_O20-5-984_S52_8_Subt10_11.")
-#'
-#' pattern3 <- paste0('sn(?<serial_number>.*) ID_(?<ID>.*)_(?<Class>[NS])',
-#'                    '(?<Sector>.*)_(?<Point>.*)_[Ss]ubt.*\\.');
-#'
-#' regexp2df(text3, pattern3)
-#'
-#' ##   serial_number    ID       Class Sector Point
-#' ## 1      555      O20-5-684     N     52     2
-#' ## 2      555      O20-5-984     S     52     8
-#'
-#' #----------------------------------------------------------
-#' # List all .R files in your working directory:
-#'
-#' regexp2df(dir(),'(?<R_file>.*\\.[rR]$)')
-#'
-#'
-#' # Do the same by using chaining operator %>%:
-#' library(dplyr)
-#'
-#' dir() %>% regexp2df('(?<R_file>\\.*[rR]$)')
-#'
-#' #----------------------------------------------------------
-#' # Capture several types of files:
-#'
-#' expr <- paste0('(?<R_file>.*\\.[rR]$)|',
-#'                '(?<Rmd_file>.*\\.[rR]md$)|',
-#'                '(?<CSV_file>.*\\.[cC][sS][vV]$)')
-#' dir() %>% regexp2df(expr)
+#' @note Call to function \code{gregexpr} with parameter \code{perl = TRUE}
+#' is used.
 #'
 #' @family \pkg{spMisc} utilities
 #' @author Vilmantas Gegzna
